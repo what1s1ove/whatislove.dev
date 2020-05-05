cf4b8bdee934102b590224177d94fbfbb58a46a5
what1s1ove <zubko.2007@gmail.com>
2020-05-05 14:06:40 +0300

    T5-Header: + toggle menu functionality

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,9 @@
 import { initColorTheme } from './theme-button/theme-button.js'
+import { initToggleHeader } from './header-button/header-button.js'
 
 const init = () => {
   initColorTheme()
+  initToggleHeader()
 }
 
 export default init
