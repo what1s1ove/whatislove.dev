ba87608e0ae3a20bbfcd3b9bdbeb06335448c57c
what1s1ove <zubko.2007@gmail.com>
2020-05-23 09:48:38 +0300

    T20-CriticalCSS: * settings default checked

--- source/js/functionality/settings/helpers.js
+++ source/js/functionality/settings/helpers.js
@@ -2,12 +2,14 @@ import { setAttribute } from '../../helpers/index'
 import { MediaBooleanTypes } from './common'
 
 const initSettingBtn = (options) => {
-  const { target, mediaQuery, attr, checkTypes, isDefaultSetAttr } = options  
+  const { target, mediaQuery, attr, checkTypes, isDefaultSetAttr } = options
 
   const storageValue = localStorage.getItem(mediaQuery)
-  const isChecked = storageValue ? MediaBooleanTypes[storageValue] : window.matchMedia(mediaQuery).matches
+  const isChecked = storageValue
+    ? MediaBooleanTypes[storageValue]
+    : window.matchMedia(mediaQuery).matches
 
-  if(storageValue || isDefaultSetAttr) {
+  if (storageValue || isDefaultSetAttr) {
     setAttribute(attr, isChecked ? checkTypes.checked : checkTypes.unchecked)
   }
 
