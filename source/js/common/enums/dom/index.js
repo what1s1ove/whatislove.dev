c3daae40007b1002d8d97523fa8d3ac021f5eedf
what1s1ove <zubko.2007@gmail.com>
2020-11-08 14:28:28 +0200

    T49-LighthouseLink: + tag name enum

--- source/js/common/enums/dom/index.js
+++ source/js/common/enums/dom/index.js
@@ -1,2 +1,3 @@
 export * from './event-listener.enum'
 export * from './form.enum'
+export * from './tag-name.enum'
---
+++ source/js/common/enums/dom/tag-name.enum.js
@@ -0,0 +1,8 @@
+const TagName = {
+  P: `P`,
+  H3: `H3`,
+  A: `A`,
+  TIME: `TIME`,
+}
+
+export { TagName }
