e609017796544a77923d9d126d6eaceeed56debb
what1s1ove <zubko.2007@gmail.com>
2021-02-13 18:18:28 +0200

    T57-FormPage: - app js file

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,12 @@
-import { initApp } from './app'
+import 'focus-visible/dist/focus-visible.min'
+import { initHeader } from './functionality/header/header'
+import { initSettings } from './functionality/settings/settings'
+import { initTimeline } from './functionality/timeline/timeline'
+
+const initApp = () => {
+  initHeader()
+  initSettings()
+  initTimeline()
+}
 
 initApp()
