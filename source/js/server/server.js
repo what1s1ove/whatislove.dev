c40e77574bee23a8823429c96ccbf90bb39847ca
what1s1ove <zubko.2007@gmail.com>
2021-02-13 12:53:57 +0200

    T40-Server: * init server

--- source/js/server/server.js
+++ source/js/server/server.js
@@ -1,17 +1,17 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
-import { HttpCode } from '~/common/enums'
 import { Database } from './database/database'
 import { initModels } from './models/models'
 import { initControllers } from './controllers/controllers'
 import { initApi } from './api/api'
-import { HttpError } from './exceptions'
-import { DatabaseName } from './common/enums'
-import { getErrorMessageResponse } from './helpers'
+import { catchError } from './middlewares'
+import { AppEvent, DatabaseName } from './common/enums'
 
 const app = new Koa()
 app.use(bodyParser())
+
+app.use(catchError)
 
 const database = new Database({
   filename: DatabaseName.DATABASE,
@@ -34,18 +34,6 @@ routes.forEach((router) => {
   app.use(router.routes()).use(router.allowedMethods())
 })
 
-app.on(`error`, (err, ctx) => {
-  const message = getErrorMessageResponse(err.message)
-
-  if (err instanceof HttpError) {
-    ctx.status = err.status
-    ctx.body = message
-
-    return
-  }
-
-  ctx.status = HttpCode.INTERNAL_SERVER_ERROR
-  ctx.body = message
-})
+app.on(AppEvent.ERROR, console.error)
 
 app.listen(3001)
