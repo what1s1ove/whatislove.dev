99fffc4f8df39139762172fd96971a7745fe2ae6
what1s1ove <zubko.2007@gmail.com>
2021-02-13 18:10:26 +0200

    T57-FormPage: + form page

--- gulp/tasks/html/html.task.mjs
+++ gulp/tasks/html/html.task.mjs
@@ -1,11 +1,14 @@
 import gulp from 'gulp'
 import htmlmin from 'gulp-htmlmin'
+import { includeFile } from '../../helpers/helpers.mjs'
 import { Config } from '../../config.mjs'
-
 
 const html = () => {
   return gulp
-    .src(`${Config.FOLDER.SOURCE}/*.html`)
+    .src([
+      `${Config.FOLDER.SOURCE}/*.html`,
+      includeFile(`${Config.FOLDER.SOURCE}/form.html`, Config.isDevelopment),
+    ])
     .pipe(
       htmlmin({
         collapseWhitespace: true,
