c10822eee342de3d7266bad99a07e67d417c523a
what1s1ove <zubko.2007@gmail.com>
2021-02-14 15:37:41 +0200

    T57-FormPage: + koa cors package

--- source/js/server/server.js
+++ source/js/server/server.js
@@ -1,16 +1,19 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors';
+import { AppConfig } from '~/common/enums'
 import { Database } from './database/database'
 import { initModels } from './models/models'
 import { initControllers } from './controllers/controllers'
 import { initApi } from './api/api'
 import { catchError } from './middlewares'
-import { AppEvent, AppConfig, DatabaseName } from './common/enums'
+import { AppEvent, DatabaseName } from './common/enums'
 
 const app = new Koa()
 app.use(bodyParser())
 app.use(catchError)
+app.use(cors())
 
 const database = new Database({
   filename: DatabaseName.DATABASE,
@@ -35,4 +38,4 @@ routes.forEach((router) => {
 
 app.on(AppEvent.ERROR, console.error)
 
-app.listen(AppConfig.PORT)
+app.listen(AppConfig.SERVER_PORT)
