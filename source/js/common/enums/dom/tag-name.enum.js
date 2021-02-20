8a738d4dbe04247581e6ee2cdcb890763bd439b4
what1s1ove <zubko.2007@gmail.com>
2021-02-20 09:18:30 +0200

    T58-RefactorIndexPage: - configure node helper

--- source/js/common/enums/dom/tag-name.enum.js
+++
@@ -1,8 +0,0 @@
-const TagName = {
-  P: `P`,
-  H3: `H3`,
-  A: `A`,
-  TIME: `TIME`,
-}
-
-export { TagName }
--- source/js/helpers/dom/index.js
+++ source/js/helpers/dom/index.js
@@ -1,3 +1,2 @@
 export * from './focus-trap.helper'
-export * from './configure-node.helper'
 export * from './get-custom-attr-name.helper'
