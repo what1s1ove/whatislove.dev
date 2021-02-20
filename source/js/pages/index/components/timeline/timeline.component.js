070b34766d2cb7e6f0c7ce20edccd2a106fecbf8
what1s1ove <zubko.2007@gmail.com>
2021-02-20 13:43:32 +0200

    T58-RefactorIndexPage: * load timelines functionality

--- source/js/pages/index/components/timeline/timeline.component.js
+++ source/js/pages/index/components/timeline/timeline.component.js
@@ -1,3 +1,5 @@
+import { Loader } from '~/components'
+import { checkIsBeforeElement } from '~/helpers'
 import { TimelineForm, TimelineList } from './components'
 import { getSuitTimelines } from './helpers'
 
@@ -5,23 +7,29 @@ class Timeline {
   constructor({ timelineApi }) {
     this._timelineApi = timelineApi
 
+    this._containerNode = null
     this._timelines = []
 
     this._handleFormChange = this._handleFormChange.bind(this)
+    this._handleTimelineShow = this._handleTimelineShow.bind(this)
 
     this._timelineFormComponent = new TimelineForm({
       onChange: this._handleFormChange,
+    })
+    this._loaderComponent = new Loader({
+      containerNode: document.querySelector(`.timeline__list-wrapper`),
     })
     this._timelineListComponent = new TimelineList()
   }
 
   async init() {
+    this._containerNode = document.querySelector(`.timeline`)
+
+    this._loaderComponent.init()
     this._timelineFormComponent.init()
     this._timelineListComponent.init()
 
-    await this._fetchTimelines()
-
-    this._renderTimelines(this._timelineFormComponent.formValues)
+    this._initListeners()
   }
 
   _renderTimelines(formValues) {
@@ -30,12 +38,32 @@ class Timeline {
     this._timelineListComponent.renderTimelines(timelines)
   }
 
-  _handleFormChange(formValues) {
-    this._renderTimelines(formValues)
+  _initListeners() {
+    document.addEventListener(`scroll`, this._handleTimelineShow)
   }
 
   async _fetchTimelines() {
     this._timelines = await this._timelineApi.getTimelines()
+  }
+
+  _handleFormChange(formValues) {
+    this._renderTimelines(formValues)
+  }
+
+  async _handleTimelineShow() {
+    const shouldLoadTimelines = checkIsBeforeElement(
+      this._containerNode.offsetTop,
+    )
+
+    if (shouldLoadTimelines) {
+      await this._fetchTimelines()
+
+      this._loaderComponent.remove()
+
+      this._renderTimelines(this._timelineFormComponent.formValues)
+
+      document.removeEventListener(`scroll`, this._handleTimelineShow)
+    }
   }
 }
 
