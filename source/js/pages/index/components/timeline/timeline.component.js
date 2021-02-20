09502ad12f5ce43604c7b1b7fae867b7b66ae788
what1s1ove <zubko.2007@gmail.com>
2021-02-20 09:22:07 +0200

    T58-RefactorIndexPage: * timeline component

--- source/js/pages/index/components/timeline/timeline.component.js
+++ source/js/pages/index/components/timeline/timeline.component.js
@@ -1,155 +1,53 @@
-import {
-  getTargetValue,
-  getFormValues,
-  changeObjectKey,
-  debounce,
-  getFormattedDate,
-  configureNode,
-} from '~/helpers'
+import { TimelineForm, TimelineList } from './components'
 
-const DEBOUNCE_DELAY = 200
-const SVG_HREF_ATTR = `xlink:href`
+const checkIsTimelineSuit = (timelineItem, filterValues) => {
+  return (
+    filterValues.skillTypes[timelineItem.skillType] &&
+    filterValues.types[timelineItem.type]
+  )
+}
 
-const timelineListNode = document.querySelector(`.timeline__list`)
-const timelineItemTemplate = document
-  .querySelector(`.timeline-item`)
-  .content.querySelector(`.timeline__item`)
-const filterFormNode = document.forms.timeline
-
-const defaultFilterSettings = getFormValues(filterFormNode.elements)
+const getSuitTimelines = (timelines, filterValues) => {
+  return timelines.filter((timeline) => {
+    return checkIsTimelineSuit(timeline, filterValues)
+  })
+}
 
 class Timeline {
   constructor({ timelineApi }) {
     this._timelineApi = timelineApi
 
-    this._onFormChange = this._onFormChange.bind(this)
-  }
+    this._timelines = []
 
-  init() {
-    this._renderTimelineItems(defaultFilterSettings)
+    this._handleFormChange = this._handleFormChange.bind(this)
 
-    filterFormNode.addEventListener(
-      `change`,
-      debounce(this._onFormChange, DEBOUNCE_DELAY),
-    )
-  }
-
-  async _renderTimelineItems(filterValues) {
-    const templates = await this._timelineApi.getTimelines()
-    const timelineItemNodes = this._getTimelineItemNodes(
-      timelineItemTemplate,
-      templates,
-      filterValues,
-    )
-
-    timelineListNode.innerHTML = ``
-
-    timelineListNode.append(...timelineItemNodes)
-  }
-
-  _onFormChange({ target }) {
-    const targetValue = getTargetValue(target)
-
-    const newFilterSettings = changeObjectKey(
-      defaultFilterSettings,
-      target.name,
-      targetValue,
-    )
-
-    this._renderTimelineItems(newFilterSettings)
-  }
-
-  _checkIsSuitableItem(timelineItem, filterValues) {
-    const isSuitable =
-      filterValues.skillTypes[timelineItem.skillType] &&
-      filterValues.types[timelineItem.type]
-
-    return isSuitable
-  }
-
-  _getReplacedIconPath(iconSourceNode, type) {
-    const replacedIconPath = iconSourceNode
-      .getAttribute(SVG_HREF_ATTR)
-      .replace(/timeline-(.*)\./, `timeline-${type}.`)
-
-    return replacedIconPath
-  }
-
-  _getTimelineItemNode(timelineItemTemplateNode, itemData) {
-    const {
-      type,
-      title,
-      origin,
-      originDesc,
-      desc,
-      link,
-      linkDesc,
-      date,
-      endDate,
-    } = itemData
-    const timelineItemNode = timelineItemTemplateNode.cloneNode(true)
-    const originLinkNode = timelineItemNode.querySelector(
-      `.timeline__item-origin`,
-    )
-    const titleNode = timelineItemNode.querySelector(`.timeline__item-title`)
-    const descriptionNode = timelineItemNode.querySelector(
-      `.timeline__item-desc`,
-    )
-    const linkNode = timelineItemNode.querySelector(`.timeline__item-link`)
-    const dateStartNode = timelineItemNode.querySelector(
-      `.timeline__dates-date--start`,
-    )
-    const dateEndNode = timelineItemNode.querySelector(
-      `.timeline__dates-date--end`,
-    )
-    const iconSourceNode = timelineItemNode.querySelector(
-      `.timeline__icon-source`,
-    )
-    const replacedIconPage = this._getReplacedIconPath(iconSourceNode, type)
-
-    configureNode(titleNode, {
-      textContent: title,
+    this._timelineFormComponent = new TimelineForm({
+      onChange: this._handleFormChange,
     })
-
-    configureNode(originLinkNode, {
-      textContent: originDesc,
-      href: origin ?? ``,
-    })
-
-    configureNode(descriptionNode, {
-      textContent: desc,
-    })
-
-    configureNode(linkNode, {
-      textContent: linkDesc,
-      href: link ?? ``,
-    })
-
-    configureNode(dateStartNode, {
-      textContent: date ? getFormattedDate(date) : ``,
-      dateTime: date ?? ``,
-    })
-
-    configureNode(dateEndNode, {
-      textContent: endDate ? `— ${getFormattedDate(endDate)}` : ``,
-      dateTime: endDate ?? ``,
-    })
-
-    iconSourceNode.setAttribute(SVG_HREF_ATTR, replacedIconPage)
-
-    return timelineItemNode
+    this._timelineListComponent = new TimelineList()
   }
 
-  _getTimelineItemNodes(timelineItemTemplateNode, timelineItems, filterValues) {
-    const timelineItemNodes = timelineItems.reduce((acc, it) => {
-      const isSuitable = this._checkIsSuitableItem(it, filterValues)
+  async init() {
+    this._timelineFormComponent.init()
+    this._timelineListComponent.init()
 
-      return isSuitable
-        ? [...acc, this._getTimelineItemNode(timelineItemTemplateNode, it)]
-        : acc
-    }, [])
+    await this._fetchTimelines()
 
-    return timelineItemNodes
+    this._renderTimelines(this._timelineFormComponent.formValues)
+  }
+
+  _renderTimelines(formValues) {
+    const timelines = getSuitTimelines(this._timelines, formValues)
+
+    this._timelineListComponent.renderTimelines(timelines)
+  }
+
+  _handleFormChange(formValues) {
+    this._renderTimelines(formValues)
+  }
+
+  async _fetchTimelines() {
+    this._timelines = await this._timelineApi.getTimelines()
   }
 }
 
