3de334f5a5356e5469d737999cc635a27445b5fa
what1s1ove <zubko.2007@gmail.com>
2021-03-19 10:56:22 +0200

    T66-LsLint: * helper name

--- source/js/helpers/boolean/index.js
+++ source/js/helpers/boolean/index.js
@@ -1,3 +1,3 @@
 export * from './check-is-one-of/check-is-one-of.helper'
 export * from './check-is-media-match/check-is-media-match.helper'
-export * from './check-is-before-element-h/check-is-before-element-h.helper'
+export * from './check-is-before-element/check-is-before-element.helper'
