ffeeb35f9d516ab88796c3ce797cf873bee6c686
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2023-12-01 16:37:47 +0200

    feat: replace all branches with main wd-239 (#241)
    * feat: replace all branches with main wd-239
    
    * ci: edit prettier lint wd-239

--- source/data/global.cjs
+++ source/data/global.cjs
@@ -1,10 +1,5 @@
-let process = require(`node:process`)
-
-let isDevelopmentMode = process.env.NODE_ENV === `development`
-
 module.exports = {
 	description: `Interface Developer`,
 	domain: `https://whatislove.dev`,
-	isDevelopmentMode,
 	title: `Vladyslav Zubko`,
 }
