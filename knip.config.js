fb0d0c3880fa0b2436abf6b1a010ddebb7718a4f
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2023-12-27 12:26:05 +0200

    ci: update knip to support 11ty and linthtml wd-282 (#286)
    * feat: add Born to Run book wd-282
    
    * ci: update knip to support 11ty and linthtml wd-282

--- knip.config.js
+++ knip.config.js
@@ -1,12 +1,10 @@
 /** @type {import('knip').KnipConfig} */
 let config = {
-	entry: [
-		`source/scripts/index.js`,
-		`eleventy.config.cjs`,
-		`.linthtmlrc.cjs`,
-	],
-	ignore: [`source/data/**/*.cjs`],
-	ignoreDependencies: [`linthtml-config-htmlacademy`],
+	eleventy: {
+		config: `eleventy.config.cjs`,
+		entry: [`source/data/**/*.cjs`],
+	},
+	entry: [`source/scripts/index.js`],
 }
 
 export default config
