addd6250661a6e8cd92c51efbf904336c97a37de
what1s1ove <zubko.2007@gmail.com>
2024-01-05 21:29:05 +0200

    chore: split tsconfigs knip wd-291

--- packages.d.ts
+++ packages.d.ts
@@ -47,25 +47,3 @@ declare module 'eslint-plugin-sonarjs' {
 		configs,
 	}
 }
-
-declare module '@11ty/eleventy' {
-	class UserConfig {
-		ignores: Set<string>
-
-		addPassthroughCopy(
-			fileOrDir: string,
-			copyOptions?: Record<string, unknown>,
-		): void
-
-		addTemplateFormats(templateFormats: string): void
-
-		addExtension(
-			fileExtension: string,
-			options: Record<string, unknown>,
-		): void
-
-		addTransform(name: string, transform: Function): void
-	}
-
-	export { UserConfig }
-}
