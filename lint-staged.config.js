4d7ae558022f2aa4484b61ebbdb99e55c4e9a08a
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2024-01-21 17:08:31 +0200

    ci: move format lint on any file change wd-321 (#333)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,10 @@
 /** @type {import('lint-staged').Config} */
 let config = {
 	'*': [
+		() => `npm run ci:lint:format`,
 		() => `npm run ci:lint:editor`,
 		() => `npm run ci:lint:fs`,
 		() => `npm run ci:lint:trash`,
-	],
-	'*.{md,json,yml,html,njk,css,js,jsx,cjs,d.ts}': [
-		() => `npm run ci:lint:format`,
 	],
 }
 
