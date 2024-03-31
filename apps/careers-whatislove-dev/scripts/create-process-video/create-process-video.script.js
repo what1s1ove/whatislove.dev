3d009a8b63a636ea222d769a029e07107f154120
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2024-03-31 17:44:15 +0300

    feat(careers-whatislove-dev): move process pieces from public folder wd-364 (#381)
    * feat(careers-whatislove-dev): move process pieces from public folder wd-364
    
    * feat(careers-whatislove-dev): add gitkeep to videos folder wd-364

--- apps/careers-whatislove-dev/scripts/create-process-video/create-process-video.script.js
+++ apps/careers-whatislove-dev/scripts/create-process-video/create-process-video.script.js
@@ -4,24 +4,25 @@ import { ProcessExitCode, getShuffledItems } from '@whatislove.dev/shared'
 import ffmpeg from 'fluent-ffmpeg'
 import { existsSync } from 'node:fs'
 import { readdir } from 'node:fs/promises'
-import { join } from 'node:path'
+import { dirname, join } from 'node:path'
 import { exit } from 'node:process'
+import { fileURLToPath } from 'node:url'
 
-let VIDEOS_FOLDER_PATH = /** @type {const} */ (`./public/videos`)
 let PROCESS_PIECE_FILE_EXT = /** @type {const} */ (`.mp4`)
 
-let processPiecePath = join(VIDEOS_FOLDER_PATH, `process-pieces`)
-let processFilePath = join(VIDEOS_FOLDER_PATH, `process.mp4`)
+let scriptDirname = dirname(fileURLToPath(import.meta.url))
+let processPiecesPath = join(scriptDirname, `./process-pieces`)
+let processFilePath = join(scriptDirname, `../../public/videos`, `process.mp4`)
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path)
 ffmpeg.setFfprobePath(ffprobeInstaller.path)
 
 /** @returns {Promise<void>} */
 let createProcessVideo = async () => {
-	let processPiecesFiles = await readdir(processPiecePath)
+	let processPiecesFiles = await readdir(processPiecesPath)
 	let processPiecesPaths = processPiecesFiles
 		.filter((file) => file.endsWith(PROCESS_PIECE_FILE_EXT))
-		.map((file) => `${processPiecePath}/${file}`)
+		.map((file) => `${processPiecesPath}/${file}`)
 	let shuffledProcessPiecesPaths = getShuffledItems(processPiecesPaths)
 
 	let processVideo = ffmpeg()
