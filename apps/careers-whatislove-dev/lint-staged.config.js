b566ac01baee3404b9a4304da97bfea933412a67
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2024-04-29 10:30:55 +0300

    ci(root): fix paths in lintstaged wd-420 (#427)

--- apps/careers-whatislove-dev/lint-staged.config.js
+++ apps/careers-whatislove-dev/lint-staged.config.js
@@ -3,7 +3,10 @@ import { default as baseConfig } from '../../lint-staged.config.js'
 /** @type {import('lint-staged').Config} */
 let config = {
 	...baseConfig,
-	'**/*.js': [() => `npm run ci:lint:js`, () => `npm run ci:lint:type`],
+	'**/*.js': [
+		() => `npm run ci:lint:js -w apps/careers-whatislove-dev`,
+		() => `npm run ci:lint:type -w apps/careers-whatislove-dev`,
+	],
 }
 
 export default config
--- packages/shared/lint-staged.config.js
+++ packages/shared/lint-staged.config.js
@@ -3,7 +3,10 @@ import { default as baseConfig } from '../../lint-staged.config.js'
 /** @type {import('lint-staged').Config} */
 let config = {
 	...baseConfig,
-	'**/*.js': [() => `npm run ci:lint:js`, () => `npm run ci:lint:type`],
+	'**/*.js': [
+		() => `npm run ci:lint:js -w packages/shared`,
+		() => `npm run ci:lint:type -w packages/shared`,
+	],
 }
 
 export default config
