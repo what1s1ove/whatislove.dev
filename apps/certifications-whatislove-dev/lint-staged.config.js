d4e5f9b9aa728a5213050311e9d41d5ae4b55e8d
what1s1ove <zubko.2007@gmail.com>
2024-04-29 10:25:07 +0300

    ci(root): fix paths in lintstaged wd-420

--- apps/certifications-whatislove-dev/lint-staged.config.js
+++ apps/certifications-whatislove-dev/lint-staged.config.js
@@ -3,8 +3,13 @@ import { default as baseConfig } from '../../lint-staged.config.js'
 /** @type {import('lint-staged').Config} */
 let config = {
 	...baseConfig,
-	'**/*.{js,jsx}': [() => `npm run ci:lint:js`, () => `npm run ci:lint:type`],
-	'**/*.css': [() => `npm run ci:lint:css`],
+	'**/*.{js,jsx}': [
+		() => `npm run ci:lint:js -w apps/certifications-whatislove-dev`,
+		() => `npm run ci:lint:type -w apps/certifications-whatislove-dev`,
+	],
+	'**/*.css': [
+		() => `npm run ci:lint:css -w apps/certifications-whatislove-dev`,
+	],
 }
 
 export default config
--- apps/whatislove-dev/lint-staged.config.js
+++ apps/whatislove-dev/lint-staged.config.js
@@ -3,9 +3,12 @@ import { default as baseConfig } from '../../lint-staged.config.js'
 /** @type {import('lint-staged').Config} */
 let config = {
 	...baseConfig,
-	'**/*.css': [() => `npm run ci:lint:css`],
-	'**/*.js': [() => `npm run ci:lint:js`, () => `npm run ci:lint:type`],
-	'**/*.njk': [() => `npm run ci:lint:bem`],
+	'**/*.css': [() => `npm run ci:lint:css -w apps/whatislove-dev`],
+	'**/*.js': [
+		() => `npm run ci:lint:js -w apps/whatislove-dev`,
+		() => `npm run ci:lint:type -w apps/whatislove-dev`,
+	],
+	'**/*.njk': [() => `npm run ci:lint:bem -w apps/whatislove-dev`],
 }
 
 export default config
