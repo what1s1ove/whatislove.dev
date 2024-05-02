e1dc50c434e57ce5aabb07cd023434f50afa64a0
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2024-05-02 15:57:16 +0300

    feat: prefer css logical properties for all apps wd-460 (#461)
    * feat(root): prefer css logical properties for all apps wd-460
    
    * feat(root): remove transform properties wd-460

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,10 @@
 /** @type {import('stylelint').Config} */
 let config = {
 	extends: [`stylelint-config-standard`, `stylelint-config-recess-order`],
+	plugins: [`stylelint-use-logical-spec`],
 	rules: {
 		'color-hex-length': `long`,
+		'liberty/use-logical-spec': `always`,
 		'selector-class-pattern': `^([a-z]*)((_|-|__|--)[a-z]+)*$`,
 	},
 }
