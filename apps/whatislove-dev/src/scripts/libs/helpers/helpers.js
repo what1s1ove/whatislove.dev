f7ce4d5003a1cd191624822e62688c12937faa6d
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2024-06-04 11:07:10 +0300

    fix(whatislove-dev): use iso date in timeline wd-545 (#546)

--- apps/whatislove-dev/src/scripts/libs/helpers/helpers.js
+++ apps/whatislove-dev/src/scripts/libs/helpers/helpers.js
@@ -2,11 +2,11 @@ export { checkIsBeforeElement } from './check-is-before-element/check-is-before-
 export { createElement } from './create-element/create-element.helper.js'
 export { fillSelectOptions } from './fill-select-options/fill-select-options.helper.js'
 export { getServerApiUrl } from './get-server-api-url/get-server-api-url.helper.js'
-export { getStringWitCheck } from './get-string-with-check/get-string-with-check.helper.js'
 export { subscribeFocusTrap } from './subscribe-focus-trap/subscribe-focus-trap.helper.js'
 export {
 	checkIsOneOf,
 	getFormattedDate,
+	getISODate,
 	getRandomNumber,
 	initDebounce,
 	setAsyncTimeout,
--- apps/whatislove-dev/src/scripts/pages/main/libs/components/timeline/libs/components/timeline-list/libs/helpers/get-timeline-template/get-timeline-template.helper.js
+++ apps/whatislove-dev/src/scripts/pages/main/libs/components/timeline/libs/components/timeline-list/libs/helpers/get-timeline-template/get-timeline-template.helper.js
@@ -1,4 +1,4 @@
-import { getFormattedDate, getStringWitCheck } from '~/libs/helpers/helpers.js'
+import { getFormattedDate, getISODate } from '~/libs/helpers/helpers.js'
 import { Timeline } from '~/modules/timeline/timeline.js'
 
 /**
@@ -9,35 +9,38 @@ let getTimelineTemplate = (timeline) => {
 	let { date, desc, endDate, link, linkDesc, origin, originDesc, title } =
 		timeline
 
-	let titleStringNode = getStringWitCheck(
-		title,
-		`<h3 class="timeline__item-title">${title}</h3>`,
-	)
-	let originStringNode = getStringWitCheck(
-		origin,
-		`<a class="timeline__item-origin" href="${origin}" target="_blank" rel="noreferrer noopener">${originDesc}</a>`,
-	)
-	let descStringNode = getStringWitCheck(
-		desc,
-		`<p class="timeline__item-description">${desc}</p>`,
-	)
-	let linkStringNode = getStringWitCheck(
-		link,
-		`<a class="timeline__item-link" href="${link}" target="_blank" rel="noreferrer noopener">${linkDesc}</a>`,
-	)
-	let endDateStringNode = getStringWitCheck(
-		endDate,
-		`<time class="timeline__item-date timeline__item-date--end" datetime="${endDate}">${getFormattedDate(endDate)}</time>`,
-	)
-	let dateStringNode = getStringWitCheck(
-		date,
+	let titleStringNode = title
+		? `<h3 class="timeline__item-title">${title}</h3>`
+		: ``
+
+	let originStringNode = origin
+		? `<a class="timeline__item-origin" href="${origin}" target="_blank" rel="noreferrer noopener">${originDesc}</a>`
+		: ``
+
+	let descStringNode = desc
+		? `<p class="timeline__item-description">${desc}</p>`
+		: ``
+
+	let linkStringNode = link
+		? `<a class="timeline__item-link" href="${link}" target="_blank" rel="noreferrer noopener">${linkDesc}</a>`
+		: ``
+
+	let endDateStringNode = endDate
+		? `
+			<time class="timeline__item-date timeline__item-date--end" datetime="${getISODate(endDate)}">
+				${getFormattedDate(endDate)}
+			</time>
 		`
-	<p class="timeline__item-dates">
-		<time class="timeline__item-date timeline__item-date--start" datetime="${date}">${getFormattedDate(date)}</time>
-		${endDateStringNode}
-	</p>
-	`,
-	)
+		: ``
+
+	let dateStringNode = date
+		? `
+			<p class="timeline__item-dates">
+				<time class="timeline__item-date timeline__item-date--start" datetime="${getISODate(date)}">${getFormattedDate(date)}</time>
+				${endDateStringNode}
+			</p>
+		`
+		: ``
 
 	return `
 		<li class="timeline__item">
