ca21719aedb83ff80de9933d6a491dc3a716d84a
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2024-06-13 21:17:03 +0300

    refactor(whatislove-dev/certifications-whatislove-dev): reuse getformatteddate helper wd-362 (#615)

--- apps/certifications-whatislove-dev/src/libs/helpers/helpers.js
+++ apps/certifications-whatislove-dev/src/libs/helpers/helpers.js
@@ -3,5 +3,5 @@ import { Font, StyleSheet } from '@react-pdf/renderer'
 let { create: createPdfStyles } = StyleSheet
 let { register: registerFonts } = Font
 
+export { getFormattedDate } from '@whatislove.dev/shared'
 export { createPdfStyles, registerFonts }
-export { getFormattedDate } from './get-formatted-date/get-formatted-date.helper.js'
