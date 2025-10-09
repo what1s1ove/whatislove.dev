8f2034eb61f94d50bc85faf09ba0892daf4d633e
Vladyslav Zubko <42296182+what1s1ove@users.noreply.github.com>
2025-10-09 15:50:28 +0300

    ci(root): update eslint import plugin wd-270 (#728)

--- packages.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'eslint-plugin-import' {
-	import { type Linter } from 'eslint'
-
-	const configs: Record<'recommended', Required<Linter.FlatConfig>>
-
-	export default {
-		configs,
-	}
-}
